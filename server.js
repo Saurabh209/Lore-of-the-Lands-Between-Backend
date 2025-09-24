14749e550ad3d83477aa8399900a388f089c9f91
Saurabh209 <saurabhhh2613@gmail.com>
2025-09-24 18:45:03 +0530

    api is now simplified further

--- server.js
+++ server.js
@@ -13,6 +13,33 @@ app.get('/api/knowledge', (req, res) => {
   res.json(Knowledge);
 });
 
+app.get('/api/knowledge/exploration', (req, res) => {
+  res.json(Knowledge[0]);
+});
+
+app.get('/api/knowledge/epicBattles', (req, res) => {
+  res.json(Knowledge[1]);
+});
+
+app.get('/api/knowledge/demiGods', (req, res) => {
+  res.json(Knowledge[2]);
+});
+
+app.get('/api/knowledge/bosses', (req, res) => {
+  res.json(Knowledge[3]);
+});
+
+app.get('/api/knowledge/lore', (req, res) => {
+  res.json(Knowledge[4]);
+});
+
+app.get('/api/knowledge/weapons', (req, res) => {
+  res.json(Knowledge[5]);
+});
+
+
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
